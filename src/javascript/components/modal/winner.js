1535e0b62fad02f7d4c60ea21ab893fe98291190
Roman Shevts <Shevts.Roman@gmail.com>
2022-06-10 11:02:28 +0300

    Implemented function showWinnerModal() in winner.js

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,16 @@
+import { showModal } from './modal';
+import { createFighterImage } from '../fighterPreview';
+import App from '../../app';
+
 export function showWinnerModal(fighter) {
   // call showModal function 
+  const title = `${fighter.name} CHAMPION!!!`;
+  const bodyElement = createFighterImage(fighter);
+  function startNewGame() {
+    const root = document.getElementById('root');
+    root.innerHTML = '';
+    App.startApp();
+  }
+
+  showModal({ title, bodyElement, onClose: () => startNewGame() });
 }
