846965a47af00c75d88784b4fed9045699596ebc
Roman Shevts <Shevts.Roman@gmail.com>
2022-06-10 10:32:11 +0300

    Prepared information and variables in fight.js

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -3,6 +3,29 @@ import { controls } from '../../constants/controls';
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
     // resolve the promise with the winner when fight is over
+    const firstPlayer = {
+      isDefense: false,
+      isCombo: false,
+      cancelAutoRepeat: false,
+      healthBar: 100,
+      ...firstFighter
+    }
+    const secondPlayer = {
+      isDefense: false,
+      isCombo: false,
+      cancelAutoRepeat: false,
+      healthBar: 100,
+      ...secondFighter
+    }
+
+    const healthBarFirstPlayer = document.getElementById('left-fighter-indicator');
+    const healthBarSecondPlayer = document.getElementById('right-fighter-indicator');
+
+    const firstPlayerCombo = new Set();
+    const secondPlayerCombo = new Set();
+
+    
+
   });
 }
 
